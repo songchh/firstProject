0c01d7d3af5fc730f0f1a6a90afafa09a464a090
songchh <song_chh@foxmail.com>
2018-04-09 17:29:57 +0800

    connect redis with php_socket

--- first.php
+++ first.php
@@ -1,2 +1,18 @@
-this is a new test.
-line two
+<?php 
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($socket, '127.0.0.1', 6379);
+
+while(1) {
+        echo "input:";
+        $input = fgets(STDIN);
+        $isQuit = preg_match('/quit/', $input);
+        socket_write($socket, $input, strlen($input));
+        $output = socket_read($socket, 1024);
+        echo $output;
+        if ($isQuit) {
+                break;
+        }
+}
+
+socket_close($socket);
